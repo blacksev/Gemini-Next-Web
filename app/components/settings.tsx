1f48d71ad56c31a92c5d69bc2ef69d1fa0c3fcc9
Deshun <liudeshun@pansoft.com>
2023-12-23 17:08:21 +0800

    feat: 移除失效链接，默认Enter发送

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -694,7 +694,7 @@ export function Settings() {
             </Popover>
           </ListItem>
 
-          <ListItem
+          {/* <ListItem
             title={Locale.Settings.Update.Version(currentVersion ?? "unknown")}
             subTitle={
               checkingUpdate
@@ -717,7 +717,7 @@ export function Settings() {
                 onClick={() => checkUpdate(true)}
               />
             )}
-          </ListItem>
+          </ListItem> */}
 
           <ListItem title={Locale.Settings.SendKey}>
             <Select
